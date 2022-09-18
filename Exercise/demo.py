4c337eb10b174418779fd2b12fa098fcbfe1bd86
Dibyaranjan Jena <45364252+dibyaranjanGIT@users.noreply.github.com>
2022-09-18 20:53:16 +0530

    update

--- Exercise/demo.py
+++ Exercise/demo.py
@@ -14,17 +14,22 @@
 # nums = [1,3,5,6]
 # print(nums.index(8))
 
+#
+# def searchInsert(nums, target: int) -> int:
+#     if target in nums:
+#         return nums.index(target)
+#     else:
+#         for i in range(len(nums)-1):
+#             f_num = nums[i]
+#             s_num = nums[i + 1]
+#             if target > f_num and target < s_num:
+#                 return i + 1
+#                 break
+#
+#
+# print(searchInsert([1,3,5,6], 7))
 
-def searchInsert(nums, target: int) -> int:
-    if target in nums:
-        return nums.index(target)
-    else:
-        for i in range(len(nums)-1):
-            f_num = nums[i]
-            s_num = nums[i + 1]
-            if target > f_num and target < s_num:
-                return i + 1
-                break
+with open("data.txt", mode="r") as file_data:
+    data= int(file_data.read())
+    print(data)
 
-
-print(searchInsert([1,3,5,6], 7))
