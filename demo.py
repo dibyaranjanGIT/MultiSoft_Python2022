75ef87316c6c611755ea3eb75e56891fb5212387
Dibyaranjan Jena <45364252+dibyaranjanGIT@users.noreply.github.com>
2022-11-27 12:18:49 +0530

    Update

--- demo.py
+++ demo.py
@@ -1,21 +1,7 @@
-word = 'geeks for geeks'
+x = 0
+def main():
+    global x
+    x = 12
 
-# print(word.find('for'))
-#
-# print(word.index('for'))
-
-print(word.count('e'))
-
-new_word = word.replace('geeks', 'dk')
-print(new_word)
-
-
-class Car:
-    def __init__(self, type):
-        self.type = type
-
-    def change_gear():
-        print('Car change 7 gear')
-
-car = Car('Sedan')
-car.change_gear()
+main()
+print(x)
--- oop/main.py
+++ oop/main.py
@@ -7,7 +7,6 @@ The constructor contains a collection of statements (i.e., instructions) that ex
 to initialize the attributes of an object.
 '''
 
-
 ### Variable scope ###
 '''
 Instance variables: If the value of a variable varies from object to object, then such variables are called instance
@@ -28,25 +27,141 @@ Class Variables: A class variable is a variable that is declared inside of class
 
 
 ### Inheritance ###
+'''
+The process of inheriting the properties of the parent class into a child class is called inheritance. 
+The existing class is called a base class or parent class and the new class is called a subclass or child class 
+or derived class.
 
-# class Polygon:
-#     pi = 3.4
-#
-#     def __init__(self):
-#         print("Thsi is polygon class")
-#
-#
-# class Triangle(Polygon):
-#
-#     def __init__(self):
-#         print("This is Triangle class")
+The main purpose of inheritance is the reusability of code because we can use the existing class to create a new class 
+instead of creating it from scratch
+'''
 
-# tg = Triangle()
-# print(tg.pi)
+
+## Single Inheritance ##
+# Base class
+# class Vehicle:
+#     def Vehicle_info(self):
+#         print('Inside Vehicle class')
+#
+# # Child class
+# class Car(Vehicle):
+#     def car_info(self):
+#         print('Inside Car class')
+#
+# # Create object of Car
+# car = Car()
+
+
+## Multiple Inheritance ##
+
+# # Parent class 1
+# class Person:
+#     def person_info(self, name, age):
+#         print('Inside Person class')
+#         print('Name:', name, 'Age:', age)
+#
+# # Parent class 2
+# class Company:
+#     def company_info(self, company_name, location):
+#         print('Inside Company class')
+#         print('Name:', company_name, 'location:', location)
+#
+# # Child class
+# class Employee(Person, Company):
+#     def employee_info(self, salary, skill):
+#         print('Inside Employee class')
+#         print('Salary:', salary, 'Skill:', skill)
+#
+# # Create object of Employee
+# emp = Employee()
+
+
+## Multilevel Inheritance ##
+
+# # Base class
+# class Vehicle:
+#     def vehicle_info(self):
+#         print('Inside Vehicle class')
+#
+# # Child class
+# class Car(Vehicle):
+#     def car_info(self):
+#         print('Inside Car class')
+#
+# # Child class
+# class SportsCar(Car):
+#     def sports_car_info(self):
+#         print('Inside SportsCar class')
+#
+# # Create object of SportsCar
+# s_car = SportsCar()
+
+
+## Hierarchical Inheritance ##
+'''
+In Hierarchical inheritance, more than one child class is derived from a single parent class. In other words, 
+we can say one parent class and multiple child classes.
+'''
+# class Vehicle:
+#     def info(self):
+#         print("This is Vehicle")
+#
+# class Car(Vehicle):
+#     def car_info(self, name):
+#         print("Car name is:", name)
+#
+# class Truck(Vehicle):
+#     def truck_info(self, name):
+#         print("Truck name is:", name)
+
+
+## Hybrid Inheritance
+'''
+When inheritance is consists of multiple types or a combination of different inheritance is called hybrid inheritance.
+'''
+# class Vehicle:
+#     def vehicle_info(self):
+#         print("Inside Vehicle class")
+#
+# class Car(Vehicle):
+#     def car_info(self):
+#         print("Inside Car class")
+#
+# class Truck(Vehicle):
+#     def truck_info(self):
+#         print("Inside Truck class")
+#
+## Sports Car can inherits properties of Vehicle and Car
+# class SportsCar(Car, Vehicle):
+#     def sports_car_info(self):
+#         print("Inside SportsCar class")
+
+
+## SUPER Function ##
+'''
+In child class, we can refer to parent class by using the super() function. The super function returns a
+temporary object of the parent class that allows us to call a parent class method inside a child class method.
+'''
+# class Company:
+#     def company_name(self):
+#         return 'Google'
+#
+# class Employee(Company):
+#     def info(self):
+#         c_name = super().company_name()
+#         print("Jessa works at", c_name)
+## Or
+# class Employee(Company):
+#     def info(self):
+#         # Calling the superclass method using class name function
+#         c_name = Company().company_name()
+#         print("Jessa works at", c_name)
+#
+# emp = Employee()
+# emp.info()
 
 
 ### Encapsulation ###
-
 '''
 Using encapsulation, we can hide an object’s internal representation from the outside. 
 This is called information hiding.
@@ -64,7 +179,7 @@ Encapsulation can be achieved by declaring the data members and methods of a cla
 But In Python, we don’t have direct access modifiers like public, private, and protected.
  We can achieve this by using single underscore and double underscores.
  
-Public Member: Accessible anywhere from otside oclass.
+Public Member: Accessible anywhere from outside class.
 Private Member: Accessible within the class (__)
 Protected Member: Accessible within the class and its sub-classes (_)
 '''
@@ -83,10 +198,11 @@ Protected Member: Accessible within the class and its sub-classes (_)
 
 
 ## Protected member ##
-# class Company:
-#     def __init__(self):
-#         self._project = "Python"
-#
+class Company:
+    def __init__(self):
+        self._project = "Python"
+        self.institute = 'MS'
+
 # # child class
 # class Employee(Company):
 #     def __init__(self, name):
@@ -97,38 +213,17 @@ Protected Member: Accessible within the class and its sub-classes (_)
 #         print("Employee name :", self.name)
 #         print("Working on project :", self._project)
 #
-# c = Employee("Jeeban")
-# c.show()
-
-
-
-### Operator Overloading ###
-# print(12 + 20)
-# print('a' + 'b')
-# here + is an example of operator overloading
-
-
-### Magic method or Dunder method ###
-
-# class Person:
-#
-#     def __init__(self, name):
-#         self.name = name
-#
-#     def __add__(self, other):
-#         return self.name  + other.name
-# #
-# p1 = Person('Dibya ')
-# p2 = Person('Jena')
-# print(p1 + p2)
-#
-# p1 = Person(20)
-# p2 = Person(20)
-# print(p1 + p2)
-
+# e = Employee("Jeeban")
+# e.show()
 
 
 ### Getter and Setter ###
+'''
+To implement proper encapsulation in Python, we need to use setters and getters. The primary purpose of using getters 
+and setters in object-oriented programs is to ensure data encapsulation. Use the getter method to access data members 
+and the setter methods to modify the data members.
+'''
+
 
 # class Student:
 #     def __init__(self, name, age):
@@ -144,7 +239,7 @@ Protected Member: Accessible within the class and its sub-classes (_)
 #     def set_age(self, age):
 #         self.__age = age
 #
-# stud = Student('Jessa', 14)
+# stud = Student('Rama', 14)
 #
 # # retrieving age using getter
 # print('Name:', stud.name, stud.get_age())
@@ -156,4 +251,33 @@ Protected Member: Accessible within the class and its sub-classes (_)
 # print('Name:', stud.name, stud.get_age())
 
 
+### Magic method or Dunder method ###
+#
+# class Employee:
+#     def __init__(self):
+#         self.name = 'Swati'
+#         self.salary = 10000
+#
+#     def __str__(self):
+#         return 'name ' + self.name + ' salary=$' + str(self.salary)
+#
+#
+# e1 = Employee()
+# print(e1)
+
+# class Person:
+#
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def __add__(self, other):
+#         return self.name + other.name
+# #
+# p1 = Person('Dibya')
+# p2 = Person('Jeeban')
+# print(p1 + p2)
+#
+# p1 = Person(20)
+# p2 = Person(20)
+# print(p1 + p2)
 
