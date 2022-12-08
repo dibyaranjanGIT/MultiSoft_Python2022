8814d00867e4f2fd41d3c78cde2f154d484d01a7
Dibyaranjan Jena <45364252+dibyaranjanGIT@users.noreply.github.com>
2022-12-08 11:11:37 +0530

    Update

---
+++ VisualStudio/vscode.py
@@ -0,0 +1,41 @@
+'''
+Visual Studio is a heavyweight IDE, which is used for component-based software development tools and for building powerful,
+ high-performance applications. Whereas the Visual Studio Code is a lightweight yet powerful IDE, that is used for building 
+ and debugging the modern web and cloud applications
+'''
+
+# 1. In the setting search mouse and check the opiton to Zoom in using control
+# 2. From the setting change the color scheme
+# 3. Install Code Runner from Jun Han to have play button to run your code
+
+# 4. In the setting search for Code Runner and check the clear previous output opttion.
+# 5. Code Runner check in the Run In Terminal option.
+# 6. Code Runner check save file before every run
+
+## Keyboard shortcuts
+# ctrl + R to run the code
+# ctrl + D to Copy Line Down
+
+
+# To activate multi line curson use the Alt key and Alt + Shift to run through it.
+
+## Custom snippets
+# Edit the code snnipts by searching python in the setting of code snippets.
+
+# Preview mode with single right click of the mouse and open the file with double
+# right click on the mouse.
+
+## Comparison of two files
+# in the powershell 'code -d 1.txt 2.txt'
+
+
+
+print("Hello World")
+a = input("Provide input :")
+print(a)
+
+
+
+
+
+
--- how_import_works.py
+++ how_import_works.py
@@ -1,2 +1,9 @@
 import sys
+
+# This will give all the list of file path where the python will look for your module.
 print(sys.path)
+
+
+
+
+
