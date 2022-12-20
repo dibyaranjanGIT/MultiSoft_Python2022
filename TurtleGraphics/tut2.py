23e4e03b94d391846d50dda5be0391d1800729af
Dibyaranjan Jena <45364252+dibyaranjanGIT@users.noreply.github.com>
2022-12-20 14:53:40 +0530

    Update

--- TurtleGraphics/tut2.py
+++ TurtleGraphics/tut2.py
@@ -1,3 +1,20 @@
+## Pen up and move forward
+
+from turtle import Turtle, Screen
+
+bob = Turtle()  # This is the new turtle class object
+bob.shape("turtle")
+
+bob.forward(20)
+bob.penup()
+bob.forward(20)
+
+
+screen = Screen()  # This screen helps us to have the turtle in the window
+screen.exitonclick()
+
+
+
 ## How to draw a dashed line
 # from turtle import Turtle, Screen
 #
@@ -12,15 +29,15 @@
 # screen.exitonclick()
 
 ## How to draw beautiful rectangle shape
-from turtle import Turtle, Screen
-tim = Turtle()
-
-num_sides = 5
-
-for i in range(num_sides):
-    angle = 360 / num_sides
-    tim.forward(100)
-    tim.right(angle)
-
-screen = Screen()
-screen.exitonclick()
+# from turtle import Turtle, Screen
+# tim = Turtle()
+#
+# num_sides = 5
+#
+# for i in range(num_sides):
+#     angle = 360 / num_sides
+#     tim.forward(100)
+#     tim.right(angle)
+#
+# screen = Screen()
+# screen.exitonclick()
---
+++ TurtleGraphics/tut3.py
@@ -0,0 +1,43 @@
+## How to set heading of your turtle and how to increate the speed
+'''
+‘fastest’ :  0
+‘fast’    :  10
+‘normal’  :  6
+‘slow’    :  3
+‘slowest’ :  1
+'''
+
+from turtle import Turtle, Screen
+
+bob = Turtle()  # This is the new turtle class object
+bob.shape("turtle")
+
+bob.forward(50)
+bob.setheading(90)
+bob.forward(50)
+bob.setheading(360)
+bob.speed('fastest')
+bob.forward(200)
+
+
+screen = Screen()  # This screen helps us to have the turtle in the window
+screen.exitonclick()
+
+## Here is a programm to generate the random walk of turtle
+# from turtle import *
+# import random
+#
+# tim = Turtle()
+# colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray",
+#            "SeaGreen"]
+# directions = [0, 90, 180, 360]
+#
+# # to increse the pensize
+# tim.pensize(10)
+# # to increase the speeed
+# tim.speed('fastest')
+#
+# for _ in range(100):
+#     tim.color(random.choice(colors))
+#     tim.forward(10)
+#     tim.setheading(random.choice(directions))
