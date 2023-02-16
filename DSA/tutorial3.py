19de2a82e55731da79611d7277de2a7de1b2b126
Dibyaranjan Jena <45364252+dibyaranjanGIT@users.noreply.github.com>
2023-02-16 10:33:27 +0530

    Update

--- DSA/tutorial3.py
+++ DSA/tutorial3.py
@@ -12,5 +12,17 @@ here the time complexity O(1)
 Brute force approach:
     run a for loop and find the count of each element in the list
 
+if the array is sorted then we can just compare the current element with next element to find the duplicates
+which will result in O(n) - Linear time complexity
 
+Sorting can be performed in two ways
+    1.Quick sort
+    2. Merge sort
+
+O(1) - Constant time
+O(log n) - Logarithim time
+O(n) - Linear time
+O(n2) - Quadratic time
+O(n3) - Cubic time
+O(2n) - Exponential time
 """
